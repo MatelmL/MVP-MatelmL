68633f4dee70dca7554907e3944d1f2eeaa573a5
Francisco Di Paolo <franciscogermandipaolo@gmail.com>
2023-07-03 14:00:07 -0300

    UnityEvents

--- Assets/Development/Scripts/GameManager.cs
+++ Assets/Development/Scripts/GameManager.cs
@@ -5,7 +5,6 @@ using System;
 public class GameManager : MonoBehaviour
 {
     public UnityEvent StartGameUE;
-    public static Action StartGameSE;
 
     //solo esta para testear, hay que sacar esto...
     private void Update()
