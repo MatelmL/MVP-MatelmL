8aa86040a78b0670990596e519be984e3a79ce1e
Robertino Papeschi <roberpapeschi@gmail.com>
2023-07-14 15:56:26 -0300

    folder reorganization

---
+++ Assets/Development/GrayBoxing/Scripts/DoorGrayBoxing.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace GrayBoxing
+{
+    public class DoorGrayBoxing : MonoBehaviour
+    {
+        public bool isActive;
+        private void OnTriggerEnter(Collider other)
+        {
+            Goblin goblin = other.gameObject.GetComponent<Goblin>();
+            if (goblin != null && isActive)
+            {
+                goblin.ToggleState();
+                goblin.gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Development/GrayBoxing/Scripts/GateHealth.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+using System.Collections.Generic;
+
+namespace GrayBoxing
+{
+    public class GateHealth : MonoBehaviour, ITakeDamage, IReset
+    {
+        public int maxLife;
+
+        public UnityEvent OnDeathUnity;
+        public UnityEvent<float> OnDamageTaken;
+        public UnityEvent<float> OnSpecialNumberReached;
+
+        public static event Action OnDeath;
+
+        public float health { get => currentLife; set => currentLife = value; }
+
+        [SerializeField] float currentLife;
+
+        [SerializeField] List<int> specialNumbers = new List<int> { 75, 50, 25 };
+        private List<int> triggeredSpecialNumbers = new List<int>();
+
+        private void Awake()
+        {
+            currentLife = maxLife;
+        }
+
+        public void TakeDamage(float damageAmount)
+        {
+            currentLife -= damageAmount;
+
+            if (currentLife <= 0)
+            {
+                currentLife = 0;
+                OnDeath?.Invoke();
+                OnDeathUnity.Invoke();
+            }
+            else
+            {
+                // En caso de necesitarse verificar el evento incluso en la muerte, ponerlo fuera del if y sacarla el else
+                OnDamageTaken?.Invoke(currentLife);
+                CheckSpecialNumberReached(specialNumbers);
+            }
+        }
+
+        public void ResetLife()
+        {
+            currentLife = maxLife;
+        }
+
+        private void CheckSpecialNumberReached(List<int> specialNumbers)
+        {
+            foreach (int specialNumber in specialNumbers)
+            {
+                if (currentLife <= specialNumber && !triggeredSpecialNumbers.Contains(specialNumber))
+                {
+                    triggeredSpecialNumbers.Add(specialNumber);
+                    OnSpecialNumberReached?.Invoke(specialNumber);
+                }
+            }
+        }
+    }
+}
