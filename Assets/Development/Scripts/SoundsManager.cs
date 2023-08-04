f86568a65dd8cab40a67e37496d9e95b77aadf20
AgustinMarceloRemeta <89120120+AgustinMarceloRemeta@users.noreply.github.com>
2023-08-04 17:25:20 -0300

    Change Sound

--- Assets/Development/Scripts/SoundsManager.cs
+++ Assets/Development/Scripts/SoundsManager.cs
@@ -5,27 +5,41 @@ using UnityEngine.Events;
 
 public class SoundsManager : MonoBehaviour
 {
-   [SerializeField] AudioSource startMusic, combatMusic, Shofar;
+    [SerializeField] List<AudioSource>  musics = new List<AudioSource>();
+   [SerializeField] AudioSource  Shofar;
     [SerializeField] float timeChange, valueChange;
 
     public static SoundsManager instance;
     private void Awake()
     {
         instance = this;
+        Door.OnDoorDie += DieSound;
     }
-    public void StartMusic()
+    void DieSound()
     {
-        StartCoroutine(ChangeMusic());
+        ChangeMusic("Source EndGame");
+    }
+    public void ChangeMusic(string newMusic)
+    {
+        AudioSource actualSourse = null;
+        foreach (AudioSource music in musics)    
+            if (music.volume >= 1)
+            {
+                actualSourse = music;
+                break;
+            }
+        StartCoroutine(ChangeMusicCoroutine(actualSourse, newMusic));
     }
 
-     IEnumerator ChangeMusic()
-     {
-        combatMusic.Play();
+     IEnumerator ChangeMusicCoroutine(AudioSource actualSourse, string newMusic)
+    {
+        AudioSource newSourse = musics.Find(c => c.name == newMusic);
+        newSourse.Play();
         yield return new WaitForSeconds(timeChange);
-        combatMusic.volume += valueChange;
-        startMusic.volume = 1 - combatMusic.volume;
-        if (combatMusic.volume >= 1) startMusic.Stop();
-        else StartCoroutine(ChangeMusic());
+        newSourse.volume += valueChange;
+        actualSourse.volume = 1 - newSourse.volume;
+        if (newSourse.volume >= 1) actualSourse.Stop();
+        else StartCoroutine(ChangeMusicCoroutine(actualSourse, newMusic));
      }
     public void PlayShofar()
     {
