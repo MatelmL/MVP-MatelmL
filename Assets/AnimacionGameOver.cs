324bf9c69a2cb6528e85526c446f96338d039ab1
Francisco Di Paolo <franciscogermandipaolo@gmail.com>
2023-08-04 19:02:59 -0300

    AnimacionFlagAndGameOver

---
+++ Assets/AnimacionGameOver.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class AnimacionGameOver : MonoBehaviour, IReset
+{
+    public GameObject targetObject;
+    public float scaleAndMoveDuration;
+    public float startAnimacion;
+    public Vector3 targetScale;
+    private Vector3 originalScale;
+
+    private void Start()
+    {
+        targetObject.SetActive(false);
+        Door.OnDoorDie += GameOver;
+
+        originalScale = targetObject.transform.localScale;
+    }
+
+    private void GameOver()
+    {
+        LeanTween.delayedCall(startAnimacion, () =>
+        {
+            targetObject.SetActive(true);
+
+            LeanTween.scale(targetObject, targetScale, scaleAndMoveDuration)
+                .setEase(LeanTweenType.easeOutSine);
+        });
+    }
+
+    public void Reset()
+    {
+        targetObject.transform.localScale = originalScale;
+        targetObject.SetActive(false);
+    }
+}
